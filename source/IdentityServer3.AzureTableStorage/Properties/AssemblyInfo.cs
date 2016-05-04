d83ac258728e9667708707c122349a6378b9d7b6
Tim Murphy <tim@26tp.com>
2016-05-05 07:08:16 +1000

    Add class project 'IdentityServer3.AzureTableStorage'

---
+++ source/IdentityServer3.AzureTableStorage/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿// todo
+
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("IdentityServer3.AzureTableStorage")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("IdentityServer3.AzureTableStorage")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
