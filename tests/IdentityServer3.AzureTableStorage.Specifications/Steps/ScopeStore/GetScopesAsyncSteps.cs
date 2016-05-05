a1627b9792825c304b72c160f3a815e14a6464ed
Tim Murphy <tim@26tp.com>
2016-05-05 17:15:14 +1000

    Write scenario 'publicOnly is true' for feature 'GetScopesAsync'

---
+++ tests/IdentityServer3.AzureTableStorage.Specifications/Steps/ScopeStore/GetScopesAsyncSteps.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace IdentityServer3.AzureTableStorage.Specifications.Steps.ScopeStore
+{
+    [Binding]
+    public class GetScopesAsyncSteps
+    {
+        [Given(@"scope table has scopes:")]
+        public void GivenScopeTableHasScopes(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"publicOnly is true")]
+        public void GivenPublicOnlyIsTrue()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"todo")]
+        public void GivenTodo()
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [When(@"ScopeStore\.GetScopesAsync\((.*)\) is called")]
+        public void WhenScopeStore_GetScopesAsyncIsCalled(string p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the result should scopes:")]
+        public void ThenTheResultShouldScopes(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
