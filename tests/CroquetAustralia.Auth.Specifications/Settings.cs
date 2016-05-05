3c05571b6b3eb9b314050534455b8df2c06cbed7
Tim Murphy <tim@26tp.com>
2016-05-06 04:06:49 +1000

    Allow project 'CroquetAustralia.Auth.Specifications' to use Azure Storage to In-memory stores

---
+++ tests/CroquetAustralia.Auth.Specifications/Settings.cs
@@ -0,0 +1,16 @@
+﻿namespace CroquetAustralia.Auth.Specifications
+{
+    internal static class Settings
+    {
+        internal static readonly bool IntegrationTests = IsDebug();
+
+        private static bool IsDebug()
+        {
+#if DEBUG
+            return true;
+#else
+            return false;
+#endif
+        }
+    }
+}
