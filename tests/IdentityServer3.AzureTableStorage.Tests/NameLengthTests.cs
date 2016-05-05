321038033cb7c0a3d688ebee575e102181dde564
Tim Murphy <tim@26tp.com>
2016-05-05 11:23:55 +1000

    Add test project 'IdentityServer3.AzureTableStorage.Tests'. See project's README.md.

---
+++ tests/IdentityServer3.AzureTableStorage.Tests/NameLengthTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+
+namespace IdentityServer3.AzureTableStorage.Tests
+{
+    public class NameLengthTests
+    {
+        [Fact]
+        public void NamesAreNotMoreThan30Chars()
+        {
+            throw new NotImplementedException("todo");
+
+            //var assembly = Assembly.GetAssembly(typeof(ClientConfigurationDbContext));
+            //var query =
+            //    from t in assembly.GetTypes()
+            //    where t.Namespace == "IdentityServer3.EntityFramework.Entities"
+            //    select t;
+            //foreach(var type in query)
+            //{
+            //    Assert.True(type.Name.Length <= 30, type.Name);
+
+            //    foreach(var prop in type.GetProperties())
+            //    {
+            //        Assert.True(prop.Name.Length <= 30, type.Name + ". " + prop.Name);
+            //    }
+            //}
+        }
+    }
+}
---
+++ tests/IdentityServer3.AzureTableStorage.Tests/Serialization/ClientConverterTests.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xunit;
+
+namespace IdentityServer3.AzureTableStorage.Tests.Serialization
+{
+    public class ClientConverterTests
+    {
+        [Fact]
+        public void CanSerializeAndDeserializeAClient()
+        {
+            throw new NotImplementedException("todo");
+            //var client = new Client{
+            //    ClientId = "123", 
+            //    Enabled = true,
+            //    AbsoluteRefreshTokenLifetime = 5, 
+            //    AccessTokenLifetime = 10, 
+            //    AccessTokenType = AccessTokenType.Jwt, 
+            //    AllowRememberConsent = true, 
+            //    RedirectUris = new List<string>{"http://foo.com"}
+            //};
+            //var clientStore = new InMemoryClientStore(new Client[]{client});
+            //var converter = new ClientConverter(clientStore);
+
+            //var settings = new JsonSerializerSettings();
+            //settings.Converters.Add(converter);
+            //var json = JsonConvert.SerializeObject(client, settings);
+
+            //var result = JsonConvert.DeserializeObject<Client>(json, settings);
+            //Assert.Same(client, result);
+        }
+    }
+}
