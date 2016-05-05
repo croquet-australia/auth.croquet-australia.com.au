63399c9f49602d5db6b59d73dc0165a2c66ae181
Tim Murphy <tim@26tp.com>
2016-05-05 10:34:32 +1000

    Add stubs to 'IdentityServer3.AzureTableStorage'

---
+++ source/IdentityServer3.AzureTableStorage/Stores/ScopeStore.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using IdentityServer3.Core.Models;
+using IdentityServer3.Core.Services;
+using Microsoft.WindowsAzure.Storage.Table;
+
+namespace IdentityServer3.AzureTableStorage.Stores
+{
+    public class ScopeStore : IScopeStore
+    {
+        private readonly CloudTable _table;
+
+        public ScopeStore(CloudTable table)
+        {
+            _table = table;
+        }
+
+        public Task<IEnumerable<Scope>> FindScopesAsync(IEnumerable<string> scopeNames)
+        {
+            throw new NotImplementedException("todo");
+        }
+
+        public Task<IEnumerable<Scope>> GetScopesAsync(bool publicOnly = true)
+        {
+            throw new NotImplementedException("todo");
+        }
+    }
+}
