5d98f931003548d948bd764585f2e508507f3488
Tim Murphy <tim@26tp.com>
2016-05-05 11:02:27 +1000

    Add test project 'IdentityServer3.AzureTableStorage.IntegrationTests'. See project's README.md.

---
+++ tests/IdentityServer3.AzureTableStorage.IntegrationTests/ClientConfigurationDbContextTests.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using Xunit;
+
+namespace IdentityServer3.AzureTableStorage.IntegrationTests
+{
+    public class ClientConfigurationDbContextTests
+    {
+        private const string ConfigConnectionStringName = "Config";
+
+        [Fact]
+        public void CanAddAndDeleteClientRedirectUri()
+        {
+            throw new NotImplementedException("todo");
+            //using (var db = new ClientConfigurationDbContext(ConfigConnectionStringName))
+            //{
+            //    db.Clients.Add(new Client
+            //    {
+            //        ClientId = "test-client",
+            //        ClientName = "Test Client"
+            //    });
+
+            //    db.SaveChanges();
+            //}
+
+            //using (var db = new ClientConfigurationDbContext(ConfigConnectionStringName))
+            //{
+            //    var client = db.Clients.First();
+
+            //    client.RedirectUris.Add(new ClientRedirectUri
+            //    {
+            //        Uri = "https://redirect-uri-1"
+            //    });
+
+            //    db.SaveChanges();
+            //}
+
+            //using (var db = new ClientConfigurationDbContext(ConfigConnectionStringName))
+            //{
+            //    var client = db.Clients.First();
+            //    var redirectUri = client.RedirectUris.First();
+
+            //    client.RedirectUris.Remove(redirectUri);
+
+            //    db.SaveChanges();
+            //}
+
+            //using (var db = new ClientConfigurationDbContext(ConfigConnectionStringName))
+            //{
+            //    var client = db.Clients.First();
+
+            //    Assert.Equal(0, client.RedirectUris.Count());
+            //}
+        }
+
+        [Fact]
+        public void CanAddAndDeleteClientScopes()
+        {
+            throw new NotImplementedException("todo");
+            //using (var db = new ClientConfigurationDbContext(ConfigConnectionStringName))
+            //{
+            //    db.Clients.Add(new Client
+            //    {
+            //        ClientId = "test-client-scopes",
+            //        ClientName = "Test Client"
+            //    });
+
+            //    db.SaveChanges();
+            //}
+
+            //using (var db = new ClientConfigurationDbContext(ConfigConnectionStringName))
+            //{
+            //    var client = db.Clients.First();
+
+            //    client.AllowedScopes.Add(new ClientScope
+            //    {
+            //        Scope = "test"
+            //    });
+
+            //    db.SaveChanges();
+            //}
+
+            //using (var db = new ClientConfigurationDbContext(ConfigConnectionStringName))
+            //{
+            //    var client = db.Clients.First();
+            //    var scope = client.AllowedScopes.First();
+
+            //    client.AllowedScopes.Remove(scope);
+
+            //    db.SaveChanges();
+            //}
+
+            //using (var db = new ClientConfigurationDbContext(ConfigConnectionStringName))
+            //{
+            //    var client = db.Clients.First();
+
+            //    Assert.Equal(0, client.AllowedScopes.Count());
+            //}
+        }
+    }
+}
