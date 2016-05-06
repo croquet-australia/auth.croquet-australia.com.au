f64359e32a5bdd20438ff8143a56407a547198a3
Tim Murphy <tim@26tp.com>
2016-05-06 20:36:15 +1000

    Replace NuGet package 'Anotar.NLog' with 'Anotar.LibLog'

--- tests/IdentityServer3.AzureTableStorage.Specifications/Properties/AssemblyInfo.cs
+++ tests/IdentityServer3.AzureTableStorage.Specifications/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 
 using System.Reflection;
 using System.Runtime.InteropServices;
-using Anotar.NLog;
+using Anotar.LibLog;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
